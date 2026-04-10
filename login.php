652a47f0c3c6729d31967fb10de634e9f5c9eb80
calvin <godspowercalvin@gmail.com>
2026-04-10 10:59:01 +0100

    Login page validation

---
+++ login.php
@@ -0,0 +1,86 @@
+<?php
+include 'config/connection.php';
+
+
+if(isLoggedIn()) {
+    if (getUserRole() === 'admin') {
+        header("Location: admin/dashboard.php");
+    } else {
+        header("Location: user/dashboard.php");
+    }
+    exit();
+}
+
+$error = '';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    if(empty($_POST["username"])) || empty($_POST["password"])) {
+        $error = "Both fields are required.";
+    } else {
+        $username = $_POST["username"];
+        $password = md5($_POST["password"]);
+        $role = $_POST["role"];
+
+        $sql = "SELECT * FROM users 
+            WHERE username='$username' AND password='$password' AND role='$role'";
+        $result = $db->query($sql);
+
+        //to check if a user is found
+        if(mysli_num_rows($result) == 1) {
+            $user = mysqli_fetch_assoc($result);
+
+            //code to store in sessiion
+            $_SESSION['user_id'] = $user['id'];
+            $_SESSION['username'] = $user['username']; 
+            $_SESSION['fullname'] = $user['fullname'];
+            $_SESSION['role'] = $user['role'];
+        }
+
+        //redirecting user to the appropriate dashboard
+        if ($role === 'admin') {
+            header("Location: admin/dashboard.php");
+        } else {
+            header("Location: user/dashboard.php");
+        }   
+        exit()
+    } else {
+        $error = "Incorrect username, password, or role.";
+    }
+}
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset ="UTF-8">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+
+        <title>Login Page</title>
+    </head>
+    <body>
+        <div class="container-fluid border">
+            <h2>Login</h2>
+            
+
+                
+            <input type ="email" id="email" placeholder="Email">
+            <input type="password" id="password" placeholder="Password">
+            
+            <div class="col">
+                <button onclick="">Login</button>
+
+
+            </div>
+            
+            
+
+        
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+
+    </body>
+</html>
+
