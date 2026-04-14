7b56f2a2342352f859a9219c27084f5e81b3c20d
calvin <godspowercalvin@gmail.com>
2026-04-14 15:32:51 +0100

    add bike handler

---
+++ admin/add_bike.php
@@ -0,0 +1,22 @@
+<?php
+require_once '../config/connection.php';
+requireAdmin();
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $name = $_POST['name'];
+    $category = $_POST['category'];
+    $price_per_day = $_POST['price_per_day'];
+    $quantity = $_POST['quantity'];
+    $serial = "BIKE" . time() . rand(100, 999);
+
+    $sql = "INSERT INTO bicycles (name, category, serial_number, price_per_day, quantity) 
+            VALUES ('$name', '$category', '$serial', $price_per_day, $quantity)";
+    
+    if ($db->query($sql)) {
+        header('Location: dashboard.php');
+    } else {
+        echo "Error: " . $db->error;
+
+    }
+}
+?>
